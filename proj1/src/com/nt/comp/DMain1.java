65bca3c4201050f5e2d613e38656e99249d311da
USER <USER@DESKTOP-84GM9DD>
2021-06-27 17:17:05 +0530

    testing code............

---
+++ proj1/src/com/nt/comp/DMain1.java
@@ -0,0 +1,13 @@
+package com.nt.comp;
+
+public class DMain1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A a=new A();
+B b=new B();
+System.out.println("sum:"+a.sum(10, 20));
+System.out.println("sub:"+b.sub(30,10));
+	}
+
+}
